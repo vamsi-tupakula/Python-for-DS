fa88aa0a58686b0ccd7164d1333d4a2584db8ef5
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-04 10:27:14 +0530

    variables added

---
+++ 01_basics_of_python/02_variables_and_datatypes.py
@@ -0,0 +1,11 @@
+'''
+    what are variables?
+    -> variables are the user defined memory locations used to store some data. or variable is a name to which a value can be assigned
+    -> we store some information in the variable and later we can access that information using the name of the variable
+'''
+
+name = "python"
+birth_year = "1991" # python was created in 1991
+
+print(name)
+print(birth_year)
