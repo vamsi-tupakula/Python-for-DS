65807e34eee579ec32e1640db67e4fc366391a46
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-04 11:11:26 +0530

    comparing operators

--- 01_basics_of_python/03_operators.py
+++ 01_basics_of_python/03_operators.py
@@ -13,3 +13,14 @@
 
 ans = 3 + 5 // 2 * 5 / (9**2)
 print(ans)
+
+'''
+    Comparision operators :
+    -> comparing operators are used to compare the values mathematically. the answer will be either true or false.
+    < > == != <= >=
+    is -> equal to
+    is not -> not equal to
+'''
+val1 = 5
+val2 = 4
+print(val1 is val2)
