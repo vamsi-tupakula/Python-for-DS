2cc7be3f76453a31b4c799052d2fd61a0b099649
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-04 11:06:32 +0530

    arithmetic operators

---
+++ 01_basics_of_python/03_operators.py
@@ -0,0 +1,15 @@
+'''
+    Operators are used to perform different operations on the variables or simple on data.
+'''
+
+'''
+    Arithmetic operators are used to perform arithmetic operations on the data, like addition, subtraction, etc...
+    order of precedence : 
+        () parenthesis
+        ** exponential
+        % module * multiplication / float division // integer division
+        + addition - subtraction
+'''
+
+ans = 3 + 5 // 2 * 5 / (9**2)
+print(ans)
