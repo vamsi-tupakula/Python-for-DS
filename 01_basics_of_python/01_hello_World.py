07251289260d685ccdb679d53eb9183f40eecb65
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-04 10:19:27 +0530

    comments in python

--- 01_basics_of_python/01_hello_World.py
+++ 01_basics_of_python/01_hello_World.py
@@ -1,1 +1,16 @@
 print("Hello, World...")
+
+# very  handy feature that all programming languages provide is commenting. we can add comments that explain the things alongside with our code
+
+# this is a single line comment
+
+"""
+this 
+is
+a multi-line comment
+"""
+
+'''
+multi-line
+comment
+'''
