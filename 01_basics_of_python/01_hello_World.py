338d5d4235b846661a23119cef0bf80b6a93673a
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-04 22:18:35 +0530

    escaping character added

--- 01_basics_of_python/01_hello_World.py
+++ 01_basics_of_python/01_hello_World.py
@@ -1,5 +1,9 @@
 print("Hello, World...")
 
+print('python\'s magic') # \ is used to escape the '
+# or
+print("python's magic") # we don't need escape character in this case
+
 # very  handy feature that all programming languages provide is commenting. we can add comments that explain the things alongside with our code
 
 # this is a single line comment
