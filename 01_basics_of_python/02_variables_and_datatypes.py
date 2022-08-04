5fc477dc5334f42bb450da5e3aa182a4d8761cac
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-04 10:52:22 +0530

    naming rules and data types added

--- 01_basics_of_python/02_variables_and_datatypes.py
+++ 01_basics_of_python/02_variables_and_datatypes.py
@@ -9,3 +9,34 @@ birth_year = "1991" # python was created in 1991
 
 print(name)
 print(birth_year)
+
+'''
+    Naming rules for variables:
+        there are some rules and conventions that need to be kept in mind for naming variables.
+        1. Name can start with upper-case or lower-case alphabet
+        2. Name can contain numbers but not in the beginning
+        3. _ can appear anywhere in the name
+        4. spaces are not allowed
+        Note : use some meaning full names instead of random characters
+'''
+
+'''
+    Datatype of a variables :
+        datatype of a variable defines the number and range of the value that the variable can have.
+        there are three main data types in python :
+        1.Number
+            Integer
+            Floating
+            Complex
+        2.String
+        3.Boolean
+'''
+
+a = 12 # integer
+b = 12.33 # float
+
+print(a)
+print(b)
+
+course = "Data Science with Python"
+print(len(course)) # to print the length of the string
