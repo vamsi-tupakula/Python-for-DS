e9a50b42b307f6c4b4780d599493a4cc71c50127
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-05 16:32:35 +0530

    loops added

---
+++ 01_basics_of_python/07_loops.py
@@ -0,0 +1,18 @@
+'''
+    Loops are used when we need to repeat same thing several number of times
+    1. for loop
+    2. while loop
+'''
+
+# for loop
+print("for loop : ")
+for i in range(5):
+    print(i+1)
+
+# while loop
+print("while loop")
+
+i = 5
+while(i > 0):
+    print(i)
+    i = i - 1
