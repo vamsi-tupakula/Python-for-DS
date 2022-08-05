3ab04163ebdd0c5bbfbae47433e088ab800f4621
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-05 18:52:42 +0530

    basics of list

---
+++ 01_basics_of_python/08_lists.py
@@ -0,0 +1,21 @@
+"""
+    Lists are used to store multiple items in a single variable. Lists are one of the 4 in-built data structures, other three are sets, dictionary, and tuples
+"""
+languages = ['python','java','c++','carbon']
+
+# printing entire list
+print(languages)
+
+# accessing individual elements
+print(languages[0])
+
+# length of the list
+print(len(languages))
+
+# -ve indexing starts from last of the list
+print(languages[-1])
+print(languages[-4])
+
+# accessing a range of values in list
+print(languages[0:2])
+print(languages[2:])
