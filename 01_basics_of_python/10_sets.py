a9fb3a0008e61d771d0a80cca105a563b504e206
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-06 18:01:47 +0530

    sets added

---
+++ 01_basics_of_python/10_sets.py
@@ -0,0 +1,7 @@
+"""
+    Sets can be considered as a the sets which we have seen in our high-school mathematics, they don't have duplicate values, they cannot be indexed and they are unordered and unchangable
+"""
+
+set_1 = {1,2,3,4,5,3}
+print(set_1)
+# it will remove duplicates automatically
