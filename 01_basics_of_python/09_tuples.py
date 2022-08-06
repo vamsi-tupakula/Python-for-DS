0df5297bec5546cf1ebf8ba3554164a895cf4287
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-06 17:48:21 +0530

    tuple methods added

--- 01_basics_of_python/09_tuples.py
+++ 01_basics_of_python/09_tuples.py
@@ -2,5 +2,13 @@
     Tuples unlike lists are immutables means we cannot make changes after creating a tuple
 '''
 
-names = ('steve','smith','corey','depp')
+names = ('steve','smith','corey','depp','steve','smith')
 print(names)
+
+# tuple methods
+# ! tuple can also have duplicate values
+# count returns the number of occurences of a value
+print(names.count('steve'))
+
+# returns the index of first appearence of a value
+print(names.index('smith'))
