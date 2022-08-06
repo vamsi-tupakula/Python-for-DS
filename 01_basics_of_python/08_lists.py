23db0901ef306e0093755ad7cbf7177f64de2a22
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-06 11:58:16 +0530

    list methods added

--- 01_basics_of_python/08_lists.py
+++ 01_basics_of_python/08_lists.py
@@ -19,3 +19,48 @@ print(languages[-4])
 # accessing a range of values in list
 print(languages[0:2])
 print(languages[2:])
+
+# list methods
+languages.append('kotlin')
+print(languages)
+
+# list can also have duplicate values
+languages.append('java')
+print(languages)
+
+# remove function will remove the first occurence of the parameter
+languages.remove('java')
+print(languages)
+
+# inserting a certain position
+languages.insert(3,'flutter')
+print(languages)
+
+# append vs extend
+languages.append(['react','react-native'])
+print(languages)
+
+languages.extend(['html','css','javascript'])
+print(languages)
+
+# remove last element
+popped = languages.pop()
+print(languages)
+print(popped)
+
+# reversing the list
+languages.reverse()
+print(languages)
+
+# removing sub-list to do sorting
+languages.remove(['react', 'react-native'])
+
+# sorting
+nums = [1,23,5,12,10]
+nums.sort()
+languages.sort()
+print(nums)
+print(languages)
+
+nums.sort(reverse=True)
+print(nums)
