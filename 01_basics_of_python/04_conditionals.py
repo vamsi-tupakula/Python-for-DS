778157ea0db711816a70956fe5b2503a735db0ab
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-06 18:51:16 +0530

    leap year added

--- 01_basics_of_python/04_conditionals.py
+++ 01_basics_of_python/04_conditionals.py
@@ -22,3 +22,8 @@ else:
     3. 0 in numeric
     4. any empty sequence '', (), {}, []
 '''
+
+def leap_year(year):
+    return (year%4 == 0 and (year%100 != 0 or year%400 == 0))
+
+print(leap_year(2020))
