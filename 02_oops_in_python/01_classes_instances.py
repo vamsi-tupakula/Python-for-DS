7fc643161b432674d767b025501d7044d82d9c90
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-11 11:29:28 +0530

    constructor added

--- 02_oops_in_python/01_classes_instances.py
+++ 02_oops_in_python/01_classes_instances.py
@@ -4,20 +4,11 @@
 """
 
 class Employee:
-    pass
+    # constructor
+    # every time we create a new instance this constructor will run.
+    def __init__(self):
+        print("Constructor is initialized....")
 
 # employee objects or instances
 emp_1 = Employee()
 emp_2 = Employee()
-print(emp_1)
-print(emp_2)
-
-# creating attributes
-emp_1.name = "Jhonny"
-emp_1.age = 23
-
-emp_2.name = "Depp"
-emp_2.age = 25
-
-print(emp_1.name)
-print(emp_2.name)
