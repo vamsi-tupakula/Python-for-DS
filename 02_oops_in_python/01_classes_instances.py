8b5053e0e1a63fc5887f277755c6a8fc76f6c731
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-11 11:26:56 +0530

    creating attributes manually

--- 02_oops_in_python/01_classes_instances.py
+++ 02_oops_in_python/01_classes_instances.py
@@ -1,3 +1,23 @@
 """
-    Classes allows use to logically group data and functions in a way that is easy to reuse and also easy to re-build
+    Classes allows use to logically group data and functions in a way that is easy to reuse and also easy to re-build.
+    Class is a blue print for creating instances.
 """
+
+class Employee:
+    pass
+
+# employee objects or instances
+emp_1 = Employee()
+emp_2 = Employee()
+print(emp_1)
+print(emp_2)
+
+# creating attributes
+emp_1.name = "Jhonny"
+emp_1.age = 23
+
+emp_2.name = "Depp"
+emp_2.age = 25
+
+print(emp_1.name)
+print(emp_2.name)
