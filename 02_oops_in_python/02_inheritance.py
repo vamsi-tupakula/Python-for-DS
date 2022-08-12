d776e31ac0c275ab6f1e8e4005fda1c759856d31
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-12 09:22:13 +0530

    Create employees from developer class

--- 02_oops_in_python/02_inheritance.py
+++ 02_oops_in_python/02_inheritance.py
@@ -12,3 +12,12 @@ class Employee:
     
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amt)
+
+class Developer(Employee):
+    pass
+
+dev_1 = Developer("Davy","Jhones", 100000)
+dev_2 = Developer("Maria","Hill", 200000)
+
+print(dev_1.email)
+print(dev_2.email)
