db4d5a438c3289e84454a56f079c1337a08507ad
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-12 09:49:51 +0530

    Manager class

--- 02_oops_in_python/02_inheritance.py
+++ 02_oops_in_python/02_inheritance.py
@@ -18,9 +18,40 @@ class Developer(Employee):
         super().__init__(first,last,pay)
         self.prog_lang = prog_lang
 
+class Manager(Employee):
+    def __init__(self,first,last,pay,employees = None):
+        super().__init__(first,last,pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+    
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emp(self):
+        print("Employees are : ")
+        for emp in self.employees:
+            print(emp.full_name())
+
 dev_1 = Developer("Davy","Jhones", 100000, "Python")
 dev_2 = Developer("Maria","Hill", 200000, "Java")
+dev_3 = Developer("Client","Barton", 150000, "C")
+dev_4 = Developer("James","Rhodes", 70000, "C++")
 
 print(dev_1.email)
 print(dev_1.prog_lang)
 print(dev_2.prog_lang)
+
+mgr_1 = Manager("Nick","Fury",3500000, [dev_1,dev_2])
+print(mgr_1.email)
+mgr_1.print_emp()
+mgr_1.add_emp(dev_3)
+mgr_1.add_emp(dev_4)
+mgr_1.remove_emp(dev_1)
+mgr_1.print_emp()
