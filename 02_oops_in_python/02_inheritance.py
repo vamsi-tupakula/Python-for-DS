b7cf48ab602354bf1027a377267abc886bf0ae76
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-12 09:33:21 +0530

    constructor in developer class

--- 02_oops_in_python/02_inheritance.py
+++ 02_oops_in_python/02_inheritance.py
@@ -14,10 +14,13 @@ class Employee:
         self.pay = int(self.pay * self.raise_amt)
 
 class Developer(Employee):
-    pass
+    def __init__(self,first,last,pay,prog_lang):
+        super().__init__(first,last,pay)
+        self.prog_lang = prog_lang
 
-dev_1 = Developer("Davy","Jhones", 100000)
-dev_2 = Developer("Maria","Hill", 200000)
+dev_1 = Developer("Davy","Jhones", 100000, "Python")
+dev_2 = Developer("Maria","Hill", 200000, "Java")
 
 print(dev_1.email)
-print(dev_2.email)
+print(dev_1.prog_lang)
+print(dev_2.prog_lang)
