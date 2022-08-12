8dfa3eddf26264e06d73a5b79b9934e7f599b34a
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-12 09:17:57 +0530

    employee class created

---
+++ 02_oops_in_python/02_inheritance.py
@@ -0,0 +1,14 @@
+class Employee:
+    raise_amt = 1.04
+
+    def __init__(self,first,last,pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + "." + last + ".company@emp.com"
+    
+    def full_name(self):
+        return f"{self.first} {self.last}"
+    
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
