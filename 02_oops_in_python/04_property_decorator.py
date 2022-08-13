c41ecfc77aaae7791465e45e7e6dbd74a0937682
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-13 16:15:50 +0530

    property decorator in employee class

---
+++ 02_oops_in_python/04_property_decorator.py
@@ -0,0 +1,15 @@
+class Employee:
+    def __init__(self,first,last):
+        self.first = first
+        self.last = last
+
+    @property
+    def email(self):
+        return f"{self.first}.{self.last}@gmail.com"
+    
+    def full_name(self):
+        return f"{self.first} {self.last}"
+
+emp_1 = Employee("Jhon","Wick")
+# print(emp_1.email()) # this will give error
+print(emp_1.email)
