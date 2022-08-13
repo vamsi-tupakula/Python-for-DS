552f55619300b92d9ed9f70ac79489826d9d00e0
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-13 15:17:42 +0530

    str and full_name methods

--- 02_oops_in_python/03_dunder_methods.py
+++ 02_oops_in_python/03_dunder_methods.py
@@ -5,8 +5,14 @@ class Employee:
         self.pay = pay
         self.email = first + "." + last + ".company@emp.com"
     
+    def full_name(self):
+        return f"{self.first} {self.last}"
+
     def __repr__(self):
         return f"Employee({self.first} {self.last}, {self.pay})"
+
+    def __str__(self):
+        return f"{self.full_name()} - {self.email}"
 
 emp_1 = Employee("John","Wick",1200000)
 print(emp_1)
