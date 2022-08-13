3e6ad07764b2bb9e3b294782870814713927b383
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-13 15:14:34 +0530

    repr function added

--- 02_oops_in_python/03_dunder_methods.py
+++ 02_oops_in_python/03_dunder_methods.py
@@ -4,6 +4,10 @@ class Employee:
         self.last = last
         self.pay = pay
         self.email = first + "." + last + ".company@emp.com"
+    
+    def __repr__(self):
+        return f"Employee({self.first} {self.last}, {self.pay})"
 
 emp_1 = Employee("John","Wick",1200000)
 print(emp_1)
+# run above line uncommenting repr function observe the difference
