64c8ea885f60258b60140bbd77d571c60e4155d0
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-13 16:18:24 +0530

    @property decorator

--- 02_oops_in_python/04_property_decorator.py
+++ 02_oops_in_python/04_property_decorator.py
@@ -7,9 +7,11 @@ class Employee:
     def email(self):
         return f"{self.first}.{self.last}@gmail.com"
     
+    @property
     def full_name(self):
         return f"{self.first} {self.last}"
 
 emp_1 = Employee("Jhon","Wick")
 # print(emp_1.email()) # this will give error
 print(emp_1.email)
+print(emp_1.full_name)
