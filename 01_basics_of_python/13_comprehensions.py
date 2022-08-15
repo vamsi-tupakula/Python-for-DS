f2ba47f5821f5668e68a643677b13d3cdd4da1e9
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-15 21:17:56 +0530

    map + lambda

--- 01_basics_of_python/13_comprehensions.py
+++ 01_basics_of_python/13_comprehensions.py
@@ -7,3 +7,7 @@ print(my_list)
 # code-block 2
 my_list = [n for n in range(11) if (n%2 == 0)]
 print(my_list)
+
+# code-block 3 (map + lambda)
+my_list = map(lambda x : x*x, range(11))
+print(list(my_list))
