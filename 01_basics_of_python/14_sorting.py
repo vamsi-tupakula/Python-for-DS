2bc3fcfc9b53c15a0be310893f8648018196467d
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-15 22:27:14 +0530

    sorting in tuples

--- 01_basics_of_python/14_sorting.py
+++ 01_basics_of_python/14_sorting.py
@@ -9,3 +9,10 @@ print(my_list)
 
 my_list.sort(reverse=True)
 print(my_list)
+
+# sorting in tuples
+# tuples does not have sort method so we need to use sorted function
+my_tuple = (9,1,8,2,7,3,6,4,5)
+sorted_tuple = sorted(my_tuple,reverse=True) 
+# by default sorted function will always returns list
+print(tuple(sorted_tuple))
