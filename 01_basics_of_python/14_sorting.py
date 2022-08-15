ad4eb9b6d1d954cb33dbe584086c1d53994f5611
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-15 22:33:11 +0530

    key attr

--- 01_basics_of_python/14_sorting.py
+++ 01_basics_of_python/14_sorting.py
@@ -10,6 +10,10 @@ print(my_list)
 my_list.sort(reverse=True)
 print(my_list)
 
+li = [-6,-5,-4,1,2,3]
+s_li = sorted(li, key=abs)
+print(s_li)
+
 # sorting in tuples
 # tuples does not have sort method so we need to use sorted function
 my_tuple = (9,1,8,2,7,3,6,4,5)
