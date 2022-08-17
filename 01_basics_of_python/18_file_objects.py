5f06d14acfb54f08ae45032f6f2f47712d2a2880
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-17 19:31:32 +0530

    printing file content

--- 01_basics_of_python/18_file_objects.py
+++ 01_basics_of_python/18_file_objects.py
@@ -1,4 +1,15 @@
-f = open('test.txt', 'r')
+# f = open('test.txt', 'r')
 
-print(f.name)
-print(f.mode)
+# print(f.name)
+# print(f.mode)
+
+# above code is same as below
+with open('test.txt', 'r') as f:
+    # print(f.name)
+    # print(f.mode)
+
+    # f_contents = f.read()
+    # print(f_contents)
+
+    for line in f:
+        print(line, end='')
