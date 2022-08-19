5e62f6c01536ef712a556fcdb900063c75ee7d94
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-19 23:47:21 +0530

    random module

---
+++ 01_basics_of_python/19_random.py
@@ -0,0 +1,10 @@
+import random
+
+value = random.random() # gives random value between 0 and 1
+print(value)
+
+value = random.uniform(1,10) # gives random float between 1 and 10
+print(value)
+
+value = random.randint(1,10) # random value between 1 and 10 including both
+print(value)
