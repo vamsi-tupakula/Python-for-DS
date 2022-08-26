460e63075c2ba14e9b111f61b2b06752c97193b4
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-26 21:26:36 +0530

    exceptions basics

--- 01_basics_of_python/21_error_handling.py
+++ 01_basics_of_python/21_error_handling.py
@@ -1,6 +1,20 @@
-a = 5
-b= 0
+# a = 5
+# b= 0
+# try:
+#     print(a//b)
+# except Exception:
+#     print('cannot divide with zero....')
+
 try:
-    print(a//b)
+    f = open('not_exists.txt')
+    print(5//0)
+    while(True):
+        print('true')
+except FileNotFoundError:
+    print('file you are looking does not exists')
+except ZeroDivisionError:
+    print('cannot divide with zero..')
+except KeyboardInterrupt:
+    print('interrupted!')
 except Exception:
-    print('cannot divide with zero....')
+    print('sorry, something went wrong...')
