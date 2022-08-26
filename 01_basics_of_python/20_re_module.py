8a238c5140a27b2bf3b40d770508581109ae761f
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-26 10:36:54 +0530

    match function

--- 01_basics_of_python/20_re_module.py
+++ 01_basics_of_python/20_re_module.py
@@ -3,7 +3,10 @@ import re
 test_string = '123abc456789abc123ABC'
 pattern = re.compile(r'abc')
 
-matches = pattern.finditer(test_string)
+# matches = pattern.finditer(test_string)
 
-for match in matches:
-    print(match)
+# for match in matches:
+#     print(match)
+
+match = pattern.match(test_string)
+print(match)
