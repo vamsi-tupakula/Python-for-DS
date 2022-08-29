0e45d91a85e474affbb540105357fe45abcddae2
Vamsi <vamsitupakula474@gmail.com>
2022-08-29 22:14:52 +0530

    passing arguments to a decorator function

--- 01_basics_of_python/22_decorators.py
+++ 01_basics_of_python/22_decorators.py
@@ -1,10 +1,12 @@
-def decorator_function(original):
-    def inner_function():
-        print('This is from decorator function')
-        return original()
-    return inner_function
+def prefix_decorator(prefix):
+    def decorator_function(original):
+        def inner_function():
+            print(prefix, 'This is from decorator function')
+            return original()
+        return inner_function
+    return decorator_function
 
-@decorator_function
+@prefix_decorator('LOG:')
 def display():
     print('hello, world...')
 
