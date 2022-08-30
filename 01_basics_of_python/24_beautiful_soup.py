9991c45ef7ac11372f209e84f6363edb9c498d47
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-30 10:28:39 +0530

    getting all the tags using for loop

--- 01_basics_of_python/24_beautiful_soup.py
+++ 01_basics_of_python/24_beautiful_soup.py
@@ -7,10 +7,17 @@ with open('simple.html') as html_file:
 # print(soup.prettify())
 match = soup.title
 match_text = soup.title.text
-print(match)
-print(match_text)
+# print(match)
+# print(match_text)
 
 match = soup.find('div', class_='footer')
-print(match)
+# print(match)
 # get h1 tag inside the footer div
-print(match.h1.text)
+# print(match.h1.text)
+
+# get all the divs
+print('---------------') # for separating
+for div in soup.find_all('div'):
+    print('Heading :', div.h1.text)
+    print('Summary :', div.p.text)
+    print('---------------') # for separating
