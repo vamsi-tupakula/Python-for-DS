82eea91f2fe82070f495d7f8660769d56766349a
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-31 18:10:33 +0530

    write header row in csv file

--- 01_basics_of_python/27_json_to_csv.py
+++ 01_basics_of_python/27_json_to_csv.py
@@ -1,6 +1,10 @@
 import json
 import csv
 
+csv_file = open('states_csv.csv', 'w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Name', 'Abbr', 'Area_codes'])
+
 # get all the data in the file
 with open('states.json') as f:
     json_data = json.load(f)
