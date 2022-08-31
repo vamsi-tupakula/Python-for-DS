e1cc0ee7e0841a263219b4f5b7ec1c565d08d467
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-31 12:41:32 +0530

    dumping into json file

--- 01_basics_of_python/26_json.py
+++ 01_basics_of_python/26_json.py
@@ -39,4 +39,8 @@ with open('states.json') as f:
     data = json.load(f)
 
 for state in data['states']:
-    print(state['name'], "--", state['abbreviation'])
+    # print(state['name'], "--", state['abbreviation'])
+    del state['area_codes']
+
+with open('new_states.json', 'w') as f:
+    json.dump(data, f, indent=2)
