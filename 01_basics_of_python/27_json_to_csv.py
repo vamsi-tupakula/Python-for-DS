535dd2d3f57e89f858ebfff1bd8bfe0976925515
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-31 18:13:46 +0530

    write all rows into csv file

--- 01_basics_of_python/27_json_to_csv.py
+++ 01_basics_of_python/27_json_to_csv.py
@@ -8,3 +8,9 @@ csv_writer.writerow(['Name', 'Abbr', 'Area_codes'])
 # get all the data in the file
 with open('states.json') as f:
     json_data = json.load(f)
+
+for state in json_data['states']:
+    csv_writer.writerow([state['name'], state['abbreviation'], state['area_codes']])
+
+print('File successfully created....')
+csv_file.close()
