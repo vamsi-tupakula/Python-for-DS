00c2e64bc6924ba033920119ad0976b166c69dce
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-08-31 12:38:34 +0530

    loading json file

--- 01_basics_of_python/26_json.py
+++ 01_basics_of_python/26_json.py
@@ -1,30 +1,42 @@
 import json
+from textwrap import indent
 
-people_string = '''
-{
-    "people": [
-        {
-            "name": "jack sparrow",
-            "phone": "2939830909",
-            "emails": ["jack@gmail.com", "sparrow@gmail.com"],
-            "has_license": false
-        },
-        {
-            "name": "iron man",
-            "phone": "29399099734",
-            "emails": null,
-            "has_license": true
-        }
-    ]
-}
-'''
+"""WORKING WITH STRINGS"""
 
-data = json.loads(people_string)
+# people_string = '''
+# {
+#     "people": [
+#         {
+#             "name": "jack sparrow",
+#             "phone": "2939830909",
+#             "emails": ["jack@gmail.com", "sparrow@gmail.com"],
+#             "has_license": false
+#         },
+#         {
+#             "name": "iron man",
+#             "phone": "29399099734",
+#             "emails": null,
+#             "has_license": true
+#         }
+#     ]
+# }
+# '''
+
+# data = json.loads(people_string)
 # print(type(data))
 # print(type(data['people']))
 # print()
-for person in data['people']:
-    del person['emails']
+# for person in data['people']:
+#     del person['emails']
 
-new_string = json.dumps(data, indent=2, sort_keys=True)
-print(new_string)
+# new_string = json.dumps(data, indent=2, sort_keys=True)
+# print(new_string)
+
+
+"""WORKING WITH JSON FILE"""
+
+with open('states.json') as f:
+    data = json.load(f)
+
+for state in data['states']:
+    print(state['name'], "--", state['abbreviation'])
