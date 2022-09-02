8d587e444ff1e215dafa87596f147593ac301782
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-02 23:56:43 +0530

    thumbnail --pillow

--- 05_pillow/pillow_demo.py
+++ 05_pillow/pillow_demo.py
@@ -7,8 +7,20 @@ image1 = Image.open('puppy_1.jpg')
 # to save the image
 # image1.save('new_assets/puppy_1.png')
 
+# changing extensions
+# for img in os.listdir('.'):
+#     if img.endswith('.jpg'):
+#         image = Image.open(img)
+#         f_name, f_ext = os.path.splitext(img)
+#         image.save('pngs/{}.png'.format(f_name))
+
+# changing aspect ratio
+
+size_300 = (300,300)
+
 for img in os.listdir('.'):
     if img.endswith('.jpg'):
         image = Image.open(img)
         f_name, f_ext = os.path.splitext(img)
-        image.save('pngs/{}.png'.format(f_name))
+        image.thumbnail(size_300)
+        image.save('300/{}_300{}'.format(f_name,f_ext))
