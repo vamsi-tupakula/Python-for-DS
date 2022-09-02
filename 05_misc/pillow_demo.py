d70d00783711f199e2c20468412936ba92a15dd3
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-02 23:46:21 +0530

    directory changed

--- 05_misc/pillow_demo.py
+++ 05_misc/pillow_demo.py
@@ -1,7 +1,12 @@
 from PIL import Image
+import os
 
-image1 = Image.open('assets/puppy_1.jpg')
+image1 = Image.open('puppy_1.jpg')
 # image1.show() # to view
 
 # to save the image
 # image1.save('new_assets/puppy_1.png')
+
+for img in os.listdir('.'):
+    if img.endswith('.jpg'):
+        print(img)
