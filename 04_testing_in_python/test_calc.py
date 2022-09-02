15541c8ed61b74266f3d8c0935c433bcf2f4171f
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-02 23:02:46 +0530

    Remaining testcases added

--- 04_testing_in_python/test_calc.py
+++ 04_testing_in_python/test_calc.py
@@ -10,8 +10,18 @@ python -m unittest test_calc.py
 
 class TestCalc(unittest.TestCase):
     def test_add(self):
-        result = calc.add(10,5)
-        self.assertEqual(result, 15)
+        self.assertEqual(calc.add(10,5), 15)
+        self.assertEqual(calc.add(-1,1), 0)
+        self.assertEqual(calc.add(-1,-1), -2)
+
+    def test_sub(self):
+        self.assertEqual(calc.sub(10,5), 5)
+
+    def test_mul(self):
+        self.assertEqual(calc.mul(10,5), 50)
+    
+    def test_divide(self):
+        self.assertEqual(calc.divide(10,5), 2)
 
 if __name__ == '__main__':
     unittest.main()
