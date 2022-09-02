3fd7dc5b9a9d0aa972cc467e6b3cf0553a0eef9e
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-02 23:58:07 +0530

    rotate method

--- 05_pillow/pillow_demo.py
+++ 05_pillow/pillow_demo.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import os
 
-image1 = Image.open('puppy_1.jpg')
+# image1 = Image.open('puppy_1.jpg')
 # image1.show() # to view
 
 # to save the image
@@ -16,11 +16,14 @@ image1 = Image.open('puppy_1.jpg')
 
 # changing aspect ratio
 
-size_300 = (300,300)
+# size_300 = (300,300)
 
-for img in os.listdir('.'):
-    if img.endswith('.jpg'):
-        image = Image.open(img)
-        f_name, f_ext = os.path.splitext(img)
-        image.thumbnail(size_300)
-        image.save('300/{}_300{}'.format(f_name,f_ext))
+# for img in os.listdir('.'):
+#     if img.endswith('.jpg'):
+#         image = Image.open(img)
+#         f_name, f_ext = os.path.splitext(img)
+#         image.thumbnail(size_300)
+#         image.save('300/{}_300{}'.format(f_name,f_ext))
+
+image1 = Image.open('puppy_1.jpg')
+image1.rotate(90).save('puppy_1_rotate.jpg')
