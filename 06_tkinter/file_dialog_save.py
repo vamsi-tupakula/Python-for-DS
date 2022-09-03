cc51cb3a1dca5991fffcce80a9bb281a27b6478b
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-03 23:04:29 +0530

    file_dialog_save.py done -tkinter

--- 06_tkinter/file_dialog_save.py
+++ 06_tkinter/file_dialog_save.py
@@ -2,7 +2,27 @@ from tkinter import *
 from tkinter import filedialog
 
 my_win = Tk()
-my_win.geometry('400x400')
+
+def save_file():
+    file = filedialog.asksaveasfile(defaultextension='.txt', filetypes=[('text file', '.txt'),('HTML file', '.html'), ('All files', '.*')])
+    text_content = str(text.get(1.0, END))
+    file.write(text_content)
+    file.close()
+
+# text area to add some text
+text = Text(my_win,
+            font=('Lucida Sans Typewriter', 22),
+            wrap=WORD,
+            height=8,
+            width=22,
+            padx=5,
+            pady=10)
+text.pack()
+
+save_btn = Button(text='Save',
+                  font=('Consolas', 14, 'bold'),
+                  command=save_file)
+save_btn.pack()
 
 my_win.resizable(False, False)
 my_win.mainloop()
