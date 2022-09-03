41f563a6fe72a60182bb0dc57c8cfcb2287d9de2
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-03 21:44:55 +0530

    filetypes and initialdir in filedialog

--- 06_tkinter/file_dialog.py
+++ 06_tkinter/file_dialog.py
@@ -2,7 +2,10 @@ from tkinter import *
 from tkinter import filedialog
 
 def select_file():
-    file_path = filedialog.askopenfilename()
+    # set initial dir path to open
+    file_path = filedialog.askopenfilename(initialdir='C:\\Users\\HP\\OneDrive\\Pictures',
+                                           title='Please choose a file..',
+                                           filetypes=(('text files', '*.txt'),('python files','*.py')))
     print(file_path)
 
 my_win = Tk()
