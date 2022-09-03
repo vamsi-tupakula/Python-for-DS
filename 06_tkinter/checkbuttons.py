7e1b075db897c151e3def28fd14de1382c8997b6
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-03 18:12:38 +0530

    checkbutton variable -tkinter

--- 06_tkinter/checkbuttons.py
+++ 06_tkinter/checkbuttons.py
@@ -3,10 +3,23 @@ import tkinter as tk
 my_win = tk.Tk()
 my_win.geometry('400x400')
 
+def display():
+    if x.get() == 0:
+        print('checkbox is equal to false')
+    else:
+        print('checkbox is equal to true')
+
+# button variable
+x = tk.IntVar()
+
 # basic checkbutton
 check_button = tk.Checkbutton(my_win,
                               text='I agree with the privacy policy.',
-                              font=('Arial', 10, 'bold'))
+                              font=('Arial', 10, 'bold'),
+                              variable=x,
+                              onvalue=1,
+                              offvalue=0,
+                              command=display)
 check_button.pack()
 
 my_win.mainloop()
