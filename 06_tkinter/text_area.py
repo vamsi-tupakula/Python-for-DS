f61c82e3d588b66aea1c8953edb4ec0ff7b0fa93
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-03 22:08:27 +0530

    text_area done -tkinter

--- 06_tkinter/text_area.py
+++ 06_tkinter/text_area.py
@@ -1,7 +1,22 @@
 from tkinter import *
 
 my_win = Tk()
-my_win.geometry('400x400')
+
+def submit():
+    print(text.get('1.0', END))
+
+# text area
+text = Text(my_win,
+            font=('Ink Free', 25),
+            height=8,
+            width=20,
+            padx=5,
+            pady=10)
+text.pack()
+
+button = Button(text='Done',
+                font=('Consolas', 13, 'bold'),
+                command=submit).pack()
 
 my_win.resizable(False, False)
 my_win.mainloop()
