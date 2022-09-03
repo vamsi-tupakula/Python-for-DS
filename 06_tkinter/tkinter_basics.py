bfe5b5f1f094051ee7032834fae9919da5dd0687
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-03 11:58:42 +0530

    change window icon -tkinter

--- 06_tkinter/tkinter_basics.py
+++ 06_tkinter/tkinter_basics.py
@@ -9,4 +9,8 @@ my_win.geometry('400x400')
 # change the window title
 my_win.title('GUI using Tkinter')
 
+# change the icon of the window
+icon = tk.PhotoImage(file='icon.png')
+my_win.iconphoto(True, icon)
+
 my_win.mainloop()
