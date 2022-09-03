7950c4866bd99c084cbc2ccaed777e681e3e41bd
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-03 11:46:29 +0530

    add window title -tkinter

--- 06_tkinter/tkinter_basics.py
+++ 06_tkinter/tkinter_basics.py
@@ -6,4 +6,7 @@ my_win = tk.Tk() # instantiate an instance of window
 # change the geometry of window
 my_win.geometry('400x400')
 
+# change the window title
+my_win.title('GUI using Tkinter')
+
 my_win.mainloop()
