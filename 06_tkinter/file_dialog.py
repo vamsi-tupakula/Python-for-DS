b4e62a49718b68f02a36ac79c3fd058f59376848
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-03 21:33:46 +0530

    askopenfilename() method -tkinter

--- 06_tkinter/file_dialog.py
+++ 06_tkinter/file_dialog.py
@@ -1,6 +1,17 @@
 from tkinter import *
+from tkinter import filedialog
+
+def select_file():
+    file_path = filedialog.askopenfilename()
+    print(file_path)
 
 my_win = Tk()
 my_win.geometry('400x400')
 
+open_btn = Button(text='Choose File',
+                  font=('Comic Sans',14),
+                  command=select_file)
+open_btn.pack()
+
+my_win.resizable(False, False)
 my_win.mainloop()
