2b1feda489278481b43d609bdb6fa6aa1c456f75
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-03 12:26:34 +0530

    change bg of label -tkinter

--- 06_tkinter/labels.py
+++ 06_tkinter/labels.py
@@ -13,7 +13,7 @@ my_win = tk.Tk()
 my_win.geometry('400x400')
 
 # creating a basic label
-label = tk.Label(my_win, text='Hey! Label', font=('Arial', 20, 'bold'), fg='#1572A1')
+label = tk.Label(my_win, text='Hey! Label', font=('Arial', 20, 'bold'), fg='#1572A1', bg='#EFDAD7')
 label.pack() # this will by default places the label at top-center
 # label.place(x=10, y=10) # this places label at certain coordinates
 
