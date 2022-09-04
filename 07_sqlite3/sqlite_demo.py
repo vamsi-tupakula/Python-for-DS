42016283903d9fe83b1fc64a8d0f260f7325f5de
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-04 18:45:25 +0530

    select query in sql

--- 07_sqlite3/sqlite_demo.py
+++ 07_sqlite3/sqlite_demo.py
@@ -12,7 +12,13 @@ c = conn.cursor()
 # conn.commit()
 
 # inserting values into table
-c.execute("INSERT INTO employees VALUES ('John','Wick', 75000)")
+# c.execute("INSERT INTO employees VALUES ('John','Wick', 75000)")
+# conn.commit()
+
+# retreive values in the table
+c.execute('SELECT * FROM employees')
+print(c.fetchone())
+
 conn.commit()
 
 conn.close()
