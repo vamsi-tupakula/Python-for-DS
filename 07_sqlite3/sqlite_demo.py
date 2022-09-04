97db14fd84113c32e9f606771aded5d4fb9f5977
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-04 19:04:20 +0530

    insert into table from Employee class

--- 07_sqlite3/sqlite_demo.py
+++ 07_sqlite3/sqlite_demo.py
@@ -1,7 +1,11 @@
 import sqlite3
+from employee import Employee
 
 conn = sqlite3.connect('employee.db')
 c = conn.cursor()
+
+# Employee class instance
+emp_1 = Employee('Chris', 'Evans', 100000)
 
 # creating employees table
 # c.execute("""CREATE TABLE employees(
@@ -15,9 +19,18 @@ c = conn.cursor()
 # c.execute("INSERT INTO employees VALUES ('John','Wick', 75000)")
 # conn.commit()
 
+# inserting employee class instance values
+c.execute("INSERT INTO employees VALUES ('{}','{}', {})".format(emp_1.first,emp_1.last,emp_1.pay))
+conn.commit()
+
+# deleting in sql
+# c.execute("DELETE FROM employees WHERE first = 'John'")
+# conn.commit()
+
 # retreive values in the table
 c.execute('SELECT * FROM employees')
-print(c.fetchone())
+# print(c.fetchone())
+print(c.fetchall())
 
 conn.commit()
 
