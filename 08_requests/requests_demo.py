18f583c96d23046bd277960973d3e1bf40cb284e
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-06 22:06:25 +0530

    passing parameters into url as params=dict

--- 08_requests/requests_demo.py
+++ 08_requests/requests_demo.py
@@ -1,7 +1,7 @@
 import requests
 
-url = 'https://xkcd.com/353/'
-res = requests.get(url)
+# url = 'https://xkcd.com/353/'
+# res = requests.get(url)
 
 # get all the methods and attributes available for res
 # print(dir(res))
@@ -10,5 +10,13 @@ res = requests.get(url)
 # print(res.text)
 
 # get status_code
-print(res.status_code)
-print(res.ok)
+# print(res.status_code)
+# print(res.ok)
+
+# passing parameters into url
+
+payload = {'page': 2, 'count': 20}
+res = requests.get('https://httpbin.org/get', params=payload)
+
+print(res.text)
+print(res.url)
