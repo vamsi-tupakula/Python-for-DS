862d0c74a82cff2845fd16484878524a0b81d70c
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-06 21:36:36 +0530

    get complete page content -requests

--- 08_requests/requests_demo.py
+++ 08_requests/requests_demo.py
@@ -4,4 +4,7 @@ url = 'https://xkcd.com/353/'
 res = requests.get(url)
 
 # get all the methods and attributes available for res
-print(dir(res))
+# print(dir(res))
+
+# get complete page content
+print(res.text)
