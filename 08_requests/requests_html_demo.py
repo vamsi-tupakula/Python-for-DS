49a3e18b93cfa4f7e2716157f23c300366e4b585
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-07 09:33:23 +0530

    get html elements using their classes and id's

--- 08_requests/requests_html_demo.py
+++ 08_requests/requests_html_demo.py
@@ -8,9 +8,16 @@ with open('sample.html') as html_file:
 # print(match[0].html)
 # print(match[0].text)
 
-matches = html.find('div')
-for match in matches:
-    print(match.text)
+# matches = html.find('div')
+# for match in matches:
+#     print(match.text)
 
-find_first = html.find('p', first=True)
-print(find_first.html)
+# find_first = html.find('p', first=True)
+# print(find_first.html)
+
+# find elements based on classes and id's
+heading__1 = html.find('#h_2__1')
+print(heading__1[0].text)
+
+paras = html.find('.paragraph')
+print(paras[1].text)
