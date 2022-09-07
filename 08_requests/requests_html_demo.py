0d80df508d46973d91fdd4d1e67d4ddf8a6e7470
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-07 09:30:14 +0530

    find all divs

--- 08_requests/requests_html_demo.py
+++ 08_requests/requests_html_demo.py
@@ -4,6 +4,13 @@ with open('sample.html') as html_file:
     source = html_file.read()
     html = HTML(html=source)
 
-match = html.find('title')
-print(match[0].html)
-print(match[0].text)
+# match = html.find('title')
+# print(match[0].html)
+# print(match[0].text)
+
+matches = html.find('div')
+for match in matches:
+    print(match.text)
+
+find_first = html.find('p', first=True)
+print(find_first.html)
