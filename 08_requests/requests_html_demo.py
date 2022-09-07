560d78d37dc0eb38bf9e0ee932e35cd1a4d5b403
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-07 09:25:25 +0530

    get elements from html --requests_html

--- 08_requests/requests_html_demo.py
+++ 08_requests/requests_html_demo.py
@@ -1,2 +1,9 @@
 from requests_html import HTML
 
+with open('sample.html') as html_file:
+    source = html_file.read()
+    html = HTML(html=source)
+
+match = html.find('title')
+print(match[0].html)
+print(match[0].text)
