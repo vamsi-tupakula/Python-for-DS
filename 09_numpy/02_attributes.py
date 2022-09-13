2dab016405392ca10efd23bbf954e876a25efd30
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-13 21:54:40 +0530

    important attributes --numpy

---
+++ 09_numpy/02_attributes.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# 2d array
+a = np.array([[1,2,3],[4,5,6]])
+print(a)
+
+# shape of the numpy array
+print('a.shape :', a.shape)
+
+# size -> returns the total number of elements in the array
+print('a.size :', a.size)
+
+# dtype -> gives the data type of elements in the array
+print('a.dtype :', a.dtype)
+
+# ndim -> returns the dimension of the array
+print('a.ndim :', a.ndim)
+
+# nbytes -> returns to size of array in bytes
+print('a.nbytes :', a.nbytes)
