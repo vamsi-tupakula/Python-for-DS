9c496f5619954b95dca9d52e14086512a6740aca
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-13 22:20:16 +0530

    array zeros method --numpy

--- 09_numpy/05_create_numpy_methods.py
+++ 09_numpy/05_create_numpy_methods.py
@@ -7,3 +7,7 @@ print(arr_ones)
 # change dtype of existing array
 arr_ones = arr_ones.astype(np.int32)
 print(arr_ones)
+
+# array zeros method
+arr_zeros = np.zeros(shape=(3,4), dtype=np.int32)
+print(arr_zeros)
