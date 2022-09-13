0081015c951d6650b62231935f3a34f27b850fae
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-13 21:43:21 +0530

    creating basic 1d array in numpy

---
+++ 09_numpy/01_numpy_getting_started.py
@@ -0,0 +1,13 @@
+"""
+Numpy is a library for python which provides support for adding large, multidimensional arrays and matrices.
+It provides high-level mathematical functions to operate on these arrays.
+Arrays, unlike python lists, is a data structure that stores a collection of objects of same datatype.
+The main data structure in numpy is Ndarray
+"""
+
+import numpy as np
+
+# 1d array
+a = np.array([1,2,3])
+print('a :', a)
+print('type(a) :', type(a))
