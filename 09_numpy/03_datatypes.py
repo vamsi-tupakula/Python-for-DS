18eb3d7d1f6e454eee50a82084e6159ffceddb93
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-13 22:05:13 +0530

    iteration in 2d arrays --numpy

--- 09_numpy/03_datatypes.py
+++ 09_numpy/03_datatypes.py
@@ -9,3 +9,16 @@ print(float_arr) # [10. 20. 30.]
 float_arr = np.array([10.0,20.0,30.0])
 print(float_arr)
 print(float_arr.dtype) #float64
+
+arr = np.array([[1,2,3],[4,5,6]])
+
+# iteration in 2d arrays :
+print(arr[0,1])
+print(arr[0][1])
+
+print('----')
+rows, cols = arr.shape
+for row in range(0,rows):
+    for col in range(0, cols):
+        print(arr[row][col], end=" ")
+    print()
