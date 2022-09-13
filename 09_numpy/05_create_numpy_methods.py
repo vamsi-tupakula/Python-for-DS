cfe567e58d71e27619dd47ca42e39f055db0472e
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-13 22:32:01 +0530

    reshape method added --numpy

--- 09_numpy/05_create_numpy_methods.py
+++ 09_numpy/05_create_numpy_methods.py
@@ -19,3 +19,8 @@ print(arr_random)
 # random.randint method
 arr_randint = np.random.randint(0,10,size=(2,4)) # 0 is inclusive and 10 is exclusive
 print(arr_randint)
+
+arr = np.array([[1,2,3],[4,5,6]])
+print(arr)
+new_arr = arr.reshape(3,2)
+print(new_arr)
