7f1f955d2ab4e5595f39c0445bd0fd6c63fbae23
Vamsi Krishna <vamsitupakula474@gmail.com>
2022-09-13 22:00:45 +0530

    creating array of dtype float --numpy

---
+++ 09_numpy/03_datatypes.py
@@ -0,0 +1,12 @@
+from pickletools import float8
+import numpy as np
+
+# creating an array of datatype float
+# method 1
+float_arr = np.array([10,20,30], dtype=np.float16)
+print(float_arr) # [10. 20. 30.]
+
+# method 2
+float_arr = np.array([10.0,20.0,30.0])
+print(float_arr)
+print(float_arr.dtype) #float64
